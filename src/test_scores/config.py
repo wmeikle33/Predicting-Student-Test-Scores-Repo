93eeee4dce81ed4bbc9011e01d42a38b08d9a92a
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-04-29 09:40:26 +0800

    Update config.py

--- src/test_scores/config.py
+++ src/test_scores/config.py
@@ -1,1 +1,9 @@
+from dataclasses import dataclass
+from pathlib import Path
 
+@dataclass
+class Config:
+    random_state: int = 42
+    artifacts_dir: Path = Path("models")
+    model_path: Path = artifacts_dir / "model.joblib"
+    pipeline_path: Path = artifacts_dir / "pipeline.joblib"
