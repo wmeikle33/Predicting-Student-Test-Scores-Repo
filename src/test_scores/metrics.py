5248fb84c9e4d1264765553e7e81e7eb14593e09
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-04-29 19:31:19 +0800

    Update metrics.py

--- src/test_scores/metrics.py
+++ src/test_scores/metrics.py
@@ -1,1 +1,12 @@
+from __future__ import annotations
 
+from typing import Any
+
+from sklearn.metrics import rmse
+
+def test_scores_metrics(y_true, y_prob) -> dict[str, float]:
+    metrics = {"rmse": float(rmse(y_true, y_prob))}
+    return metrics
+
+def metric_score(metric_fn: Any, y_true, y_pred):
+    return metric_fn(y_true, y_pred)
